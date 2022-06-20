7eb226e3f8eab88f96ad10c74799a422a393d52b
Aaron Fowlkes <anbfowlkes@gmail.com>
2022-06-20 15:06:56 -0400

    basic coloring and shapes

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const header2 = document.createElement('h2');
-header2.textContent = "This header was added using JavaScript";
+header2.textContent = "This website uses HTML, CSS, and Javascript";
 document.querySelector('body').appendChild(header2);
